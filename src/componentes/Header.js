34420c5863cc5822ec2d71a25548b78cb5b87fa2
Gozaro <gzaragoza@gmail.com>
2019-04-30 22:14:29 +0200

    Initial commit

---
+++ src/componentes/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <header className="uk-margin" uk-margin="true">
+
+                    <h1 className="uk-text-center">Eventos de Eventbrite</h1>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Header;
